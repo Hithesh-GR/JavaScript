7e43a085f4ec2edec1c0e39c9f9e7b5c057f9644
Hithesh-GR <hithu.gowda2@gmail.com>
2019-01-19 16:03:47 +0530

    first commit

---
+++ OopsPrograms/Utility/utility.js
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node fileName.js                 
+ *  Purpose         : TO deploy all the business logic.
+ *  @file           : utility.js
+ *  @overview       : ALl the business logic bus be here.
+ *  @module         : read-line modules are installed
+ *  @author         : HITHESH G R
+ *  @version        : v8.15.0
+ *  @since          : 11-01-2019
+ ******************************************************************************/
+var filestream = require('fs');
+var readline = require('readline-sync');
+module.exports = {
+    
+}
